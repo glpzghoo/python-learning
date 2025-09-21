70c1a68621c97e5209a640efc1a438ebb3ccd724
glpzghoo <my.as.glpzghoo@gmail.com>
2025-09-21 13:07:27 +0800

    exercise 45

---
+++ 41-50/45-count-lines-of-file.py
@@ -0,0 +1,12 @@
+lines = 0
+words = 0
+characters = 0
+
+with open('sample-text.txt', 'r') as file:
+    for line in file:
+        lines += 1
+        words += len(line.split())
+        characters += len(line)
+
+
+print(lines, words, characters)
